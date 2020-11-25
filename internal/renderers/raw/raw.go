fe9ba4380d78fadcbeb01c68c3ed9745304131f2
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2020-11-26 00:39:09 +0100

    renderers: raw: send filename for inline data too

--- internal/renderers/raw/raw.go
+++ internal/renderers/raw/raw.go
@@ -15,12 +15,15 @@ func (h *RawRenderer) Supports(mimetype string) bool {
 }
 
 func (h *RawRenderer) Render(w http.ResponseWriter, r *http.Request, fd *filedata.FileData) error {
-	w.Header().Set("Content-Type", fd.Mimetype)
-	w.Header().Set("X-Content-Type-Options", "nosniff")
-
-	if !(strings.HasPrefix(fd.Mimetype, "audio/") || strings.HasPrefix(fd.Mimetype, "image/") || strings.HasPrefix(fd.Mimetype, "video/")) {
-		w.Header().Set("Content-Disposition", fmt.Sprintf(`attachment; filename="%s"`, fd.GetFilename()))
+	disposition := "attachment"
+	mt := strings.Split(fd.Mimetype, "/")
+	if len(mt) > 0 && (mt[0] == "audio" || mt[0] == "image" || mt[0] == "video") {
+		disposition = "inline"
 	}
+
+	w.Header().Set("Content-Type", fd.Mimetype)
+	w.Header().Set("Content-Disposition", fmt.Sprintf(`%s; filename="%s"`, disposition, fd.GetFilename()))
+	w.Header().Set("X-Content-Type-Options", "nosniff")
 
 	return fd.ServeData(w, r)
 }
